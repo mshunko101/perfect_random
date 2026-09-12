31b7ef987151df7ace04e316b10f826e28bb3d65
Шунько М.Г. <155988585+mshunko101@users.noreply.github.com>
2026-09-12 20:30:23 +0300

    Update framework.h

--- src/framework.h
+++ src/framework.h
@@ -1,7 +1,7 @@
 ﻿#pragma once
 
 #ifndef VC_EXTRALEAN
-#define VC_EXTRALEAN            // Исключите редко используемые компоненты из заголовков Windows
+#define VC_EXTRALEAN            // Исключите редко используемые компоненты из заголовков Windows!
 #endif
 
 #include "targetver.h"
